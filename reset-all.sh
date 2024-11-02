e8d84af42ccba0c075bcd6a8fefd42018b8777a8
Razvan Deaconescu <razvand@unikraft.io>
2024-11-02 12:50:16 +0200

    Introduce reset-all.sh script
    The `reset-all.sh` script resets all Docker container configuration. It
    is to be used in case of issues that require restoring the initial
    setup.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

---
+++ reset-all.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+container_stop_and_remove()
+{
+    name="$1"
+
+    # Check if container is running.
+    if test "$(docker container inspect -f '{{.State.Running}}' "$name" 2> /dev/null)" = "true"; then
+        # Stop container if it running.
+        echo "Stopping container $name ... "
+        docker stop "$name"
+        echo "DONE"
+    fi
+
+    # Check if container exists.
+    docker container inspect "$name" > /dev/null 2>&1
+    if test $? -eq 0; then
+        # Remove container if it exists.
+        echo "Removing container $name ... "
+        docker rm "$name"
+    fi
+}
+
+container_stop_and_remove "cdl-nginx"
+container_stop_and_remove "ctf-piece_of_pie"
+
+# Start Nginx container
+./vanilla-nginx/run-nginx-container.sh
+
+# Start CTF container
+cd ctf/deploy
+make run
